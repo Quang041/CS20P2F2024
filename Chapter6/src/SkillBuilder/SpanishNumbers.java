602e0d442d08f5e7d7ccda3f42b17df1330ad806
1100101825 <1100101825@L815WD220017.edu.cbe.ab.ca>
2024-10-29 09:31:33 -0600

    9:31AM

---
+++ Chapter6/src/SkillBuilder/SpanishNumbers.java
@@ -0,0 +1,26 @@
+package SkillBuilder;
+import java.util.*;
+public class SpanishNumbers {
+
+	public static void SpanishNumbers() {
+		System.out.println(" 1 uno");
+		System.out.println(" 2 dos");
+		System.out.println(" 3 tres");
+		System.out.println(" 4 cuatro");
+		System.out.println(" 5 cinco");
+		System.out.println(" 6 sies");
+		System.out.println(" 7 siete");
+		System.out.println(" 8 ocho");
+		System.out.println(" 9 nueve");
+		System.out.println(" 10 diez");
+		
+	}
+	public static void main(String[] args) {
+		for (int i = 0; i<=10; i++) {
+			SpanishNumbers();
+			System.out.println("");
+			System.out.println("");
+		}
+	}}
+
+
