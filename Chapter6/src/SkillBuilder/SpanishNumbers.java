252de0419d955dc16d0202ac8b154df19794121f
Quang041 <nguyenquangn68@gmail.com>
2024-11-15 10:29:18 -0700

    10:29AM

--- Chapter6/src/SkillBuilder/SpanishNumbers.java
+++ Chapter6/src/SkillBuilder/SpanishNumbers.java
@@ -16,11 +16,64 @@ public class SpanishNumbers {
 		
 	}
 	public static void main(String[] args) {
-		for (int i = 0; i<=10; i++) {
-			SpanishNumbers();
-			System.out.println("");
-			System.out.println("");
+		Scanner userinput = new Scanner (System.in);
+		SpanishNumbers();
+		System.out.print("Enter a number from 1 to 10: ");
+		int user = userinput.nextInt();
+		
+		switch (user) {
+		case 1: 
+			for (int i = 0; i<10; i++) {
+				System.out.println("Uno");
+			}
+			break;
+		case 2:
+			for (int i = 0; i<10; i++) {
+				System.out.println("Dos");
+			}
+			break;
+		case 3:
+			for (int i = 0; i<10; i++) {
+				System.out.println("Tres");
+			}
+			break;
+		case 4:
+			for (int i = 0; i<10; i++) {
+				System.out.println("Cuatro");
+			}
+			break;
+		case 5:
+			for (int i = 0; i<10; i++) {
+				System.out.println("Cinco");
+			}
+			break;
+		case 6:
+			for (int i = 0; i<10; i++) {
+				System.out.println("Sies");
+			}
+			break;
+		case 7:
+			for (int i = 0; i<10; i++) {
+				System.out.println("Siete");
+			}
+			break;
+		case 8:
+			for (int i = 0; i<10; i++) {
+				System.out.println("Ocho");
+			}
+			break;
+		case 9: 
+			for (int i = 0; i<10; i++) {
+				System.out.println("Nueve");
+			}
+			break;
+		case 10:
+			for (int i = 0; i<10; i++) {
+				System.out.println("Diez");
+			}
+			break;
+		default:
+			System.out.print("Invalid| Enter a number from 1 to 10");
 		}
 	}}
-
 
